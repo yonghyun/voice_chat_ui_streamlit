2ed75dc81e8ccd42f266d0d3101c12fdcb3ce9a7
yonghyun <younghyun347@gmail.com>
2024-08-02 11:09:43 +0900

    test1

---
+++ streamlit_test.py
@@ -0,0 +1,18 @@
+# 실행방법
+# streamlit run streamlit streamlit_test.py
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+print(chart_data)
+
+c = (
+   alt.Chart(chart_data)
+   .mark_circle()
+   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
